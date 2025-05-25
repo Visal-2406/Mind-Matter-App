147fc2cb2e9e589a06290b2d51d0a6ee21fad80c
Visal-2406 <visalvs2406@gmail.com>
2025-05-25 15:18:46 +0530

    Update ResponseModel.js

--- server/models/ResponseModel.js
+++ server/models/ResponseModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const PHQ9ResponseSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    date: { type: String, required: true }, // Use ISO date string (e.g., "2025-01-13")
+    date: { type: String, required: true }, // Use ISO date string (e.g., "2025-05-24")
     responses: { type: [Number], required: true }, // Array of 9 responses (0–3)
     score: { type: Number, required: true },
     severity: { type: String, required: true },
