6ffeadd8b0d241fa80a26b0152c00afe70a7cbb3
Visal-2406 <visalvs2406@gmail.com>
2025-05-26 14:08:51 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express()
 const dotenv = require('dotenv').config()
 const cors = require('cors')
 const connectDB = require('./config/db')
-const port = process.env.PORT
+const port = process.env.PORT || 5000
 const phq9Routes = require('./routes/phq9Routes')
 const userRoutes = require('./routes/userRoute')
 
@@ -11,7 +11,13 @@ const userRoutes = require('./routes/userRoute')
 
 connectDB()
 
-app.use(cors())
+const corsOptions = {
+      origin: 'https://mind-matter-app.vercel.app', 
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      credentials: true
+    };
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
@@ -21,8 +27,14 @@ app.use('/api/phq9', phq9Routes);
 app.use('/api/user', userRoutes)
 
 app.use((err, req, res, next) => {
-  res.status(res.statusCode || 500).json({ message: err.message });
-});
+      console.error(err.stack); 
+      const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+      res.status(statusCode).json({
+        message: err.message,
+        
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack
+      });
+    });
 
 app.listen(port, () => {
     console.log(`Server started at ${port}`)
